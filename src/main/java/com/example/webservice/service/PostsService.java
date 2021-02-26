ae77fb3d1fe5d9ec6c0f00e5d19b1f48d68beaa7
rltn2121 <54628612+rltn2121@users.noreply.github.com>
2021-02-26 10:36:42 +0900

    화면 추가

--- src/main/java/com/example/webservice/service/PostsService.java
+++ src/main/java/com/example/webservice/service/PostsService.java
@@ -2,6 +2,7 @@ package com.example.webservice.service;
 
 import com.example.webservice.domain.posts.PostRepository;
 import com.example.webservice.domain.posts.Posts;
+import com.example.webservice.web.dto.PostsListResponseDto;
 import com.example.webservice.web.dto.PostsResponseDto;
 import com.example.webservice.web.dto.PostsSaveRequestDto;
 import com.example.webservice.web.dto.PostsUpdateRequestDto;
@@ -9,17 +10,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class PostsService {
     private final PostRepository postRepository;
 
+    // 등록
     public Long save(PostsSaveRequestDto requestDto){
         // Dto를 entity로 변경해서 저장
         return postRepository.save(requestDto.toEntity()).getId();
     }
 
+    // 수정
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
@@ -27,10 +34,24 @@ public class PostsService {
         return id;
     }
 
+    // 조회
     public PostsResponseDto findById(Long id) {
         Posts posts = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
         // 엔티티로 조회해서 dto로 리턴
         return new PostsResponseDto(posts);
+    }
+
+    public void delete(Long id){
+        Posts posts = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
+        postRepository.delete(posts);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/example/webservice/web/PostsApiController.java
+++ src/main/java/com/example/webservice/web/PostsApiController.java
@@ -13,7 +13,7 @@ public class PostsApiController {
     private final PostsService postsService;
 
     // 등록
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
@@ -24,6 +24,12 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    // 삭제
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
     // 조회
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
